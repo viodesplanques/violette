988a9f15119b84fcdfd2d4e4f30cfa74532219ae
Margueri <72605846+Margueri@users.noreply.github.com>
2020-11-18 14:04:20 +0100

    Add files via upload

--- Modele.py
+++ Modele.py
@@ -7,9 +7,7 @@ Created on Wed Nov 11 13:44:10 2020
 
 from matplotlib import pyplot as plt
 import numpy as np 
-import matplotlib.cbook as cbook
-import matplotlib.colors as colors
-import random as rand
+import math
 
 fig, ax = plt.subplots(figsize=(18,12))
 
@@ -56,18 +54,32 @@ California.yaxis.set_visible(False)
 #LA= California.scatter(6411.29,1061.80)
 
 Spirale = plt.subplot2grid((6, 6), (1, 1), rowspan=2, colspan=2) 
-p= 2
-angle=0
-x=[]
-y=[]
-for i in range(0, 1000):
-    a=p*np.cos(angle)
-    b=p*np.sin(angle)
-    x.append(a)
-    y.append(b)
-    p+=2
-    angle+=np.pi/100
-Spirale.plot(x,y)
+xplas= 3205.64
+yplas= 2732
+xpart=4808.46
+ypart=1061
+diffx= xpart-xplas
+diffy= ypart-yplas
+p= math.sqrt(diffx**2+diffy**2)
+if diffx>=0 and diffy>=0:
+    angle=math.acos(abs(diffx)/p)
+elif diffx<0 and diffy>=0:
+    angle=np.pi/2 + math.acos(abs(diffx)/p)
+elif diffx<0 and diffy<0:
+    angle= np.pi +math.acos(abs(diffx)/p)
+elif diffx>=0 and diffy<0:
+    angle=3*np.pi/2+math.acos(abs(diffx)/p)
+x=[xpart]
+y=[ypart]
+while p>200:
+    p-= p/20
+    angle -= np.pi/10
+    xpart = xplas+p*np.cos(angle)
+    ypart = yplas+p*np.sin(angle)
+    x.append(xpart)
+    y.append(ypart)
+plt.plot(x,y)
+plt.show()
 
 Spirale.set_title('Spirale')
 Spirale.xaxis.set_visible(False)
