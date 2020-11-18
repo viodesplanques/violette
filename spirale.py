73b06549ad786ce1606f5841d4ef5ca90c4016f6
viodesplanques <72859907+viodesplanques@users.noreply.github.com>
2020-11-18 14:01:16 +0100

    spiral corrigé

--- spirale.py
+++ spirale.py
@@ -1,17 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-p= 2
-angle=0
-x=[]
-y=[]
-for i in range(0, 1000):
-    a=p*np.cos(angle)
-    b=p*np.sin(angle)
-    x.append(a)
-    y.append(b)
-    p+=2
-    angle+=np.pi/100
+import math
+xplas= 3205.64
+yplas= 2732
+xpart=4808.46
+ypart=1061
+diffx= xpart-xplas
+diffy= ypart-yplas
+p= math.sqrt(diffx**2+diffy**2)
+if diffx>=0 and diffy>=0:
+    angle=math.acos(abs(diffx)/p)
+elif diffx<0 and diffy>=0:
+    angle=np.pi/2 + math.acos(abs(diffx)/p)
+elif diffx<0 and diffy<0:
+    angle= np.pi +math.acos(abs(diffx)/p)
+elif diffx>=0 and diffy<0:
+    angle=3*np.pi/2+math.acos(abs(diffx)/p)
+x=[xpart]
+y=[ypart]
+while p>200:
+    p-= p/20
+    angle -= np.pi/10
+    xpart = xplas+p*np.cos(angle)
+    ypart = yplas+p*np.sin(angle)
+    x.append(xpart)
+    y.append(ypart)
 plt.plot(x,y)
 plt.show()
  
